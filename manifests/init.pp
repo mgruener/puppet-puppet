45db6cef70f843490f8c90ee7e1611b4d9ede3bc
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-07 22:51:56 +0100

    Add the option to provide a hash of puppet configuration options to the puppet class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,7 @@
-class puppet {
+class puppet ( $config = hiera_hash("${module_name}::config",undef)) {
   include puppet::agent
+
+  if $config != undef {
+    create_resources('puppet::config',$config)
+  }
 }
