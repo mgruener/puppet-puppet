a43f07c55261f8875a8dce3701af5e7b47afca62
root <root@chaoshost.chaos>
2013-12-07 19:54:24 +0100

    Add puppet master class

---
+++ manifests/master.pp
@@ -0,0 +1,24 @@
+class puppet::master ($ensure = hiera("${module_name}::master::ensure",running),
+                      $enable = hiera("${module_name}::master::enable",true)) {
+
+  validate_bool($enable)
+
+  package { 'puppet-server':
+    ensure => present
+  }
+
+  case $::operatingsystem {
+    'Fedora': { $servicename = 'puppetmaster.service'
+                $serviceprovider = systemd
+    }
+    default: {  $servicename = 'puppetmaster'
+                $serviceprovider = undef
+    }
+  }
+
+  service { $servicename:
+    ensure   => $ensure,
+    enable   => $enable,
+    provider => $serviceprovider,
+  }
+}
